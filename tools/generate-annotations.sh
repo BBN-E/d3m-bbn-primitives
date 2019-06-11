2c3d2481971c929dfae0ac1bf7daeabcb49428ed
chinnu pittapally <cpittapa@d414.bbn.com>
2019-06-11 11:13:53 -0400

    update generate annotations

--- tools/generate-annotations.sh
+++ tools/generate-annotations.sh
@@ -6,21 +6,21 @@ PYTHON=python3.6
 TARDIR=`pwd`/annotations
 SRC_PIPELINES_DIR=/validation_output
 
-#primitives=$(python3.6 -m d3m.index search 2>/dev/null| grep d3m.primitives.bbn)
-primitives="d3m.primitives.data_preprocessing.csv_reader.CSVReader
-d3m.primitives.data_preprocessing.audio_reader.AudioReader
-d3m.primitives.data_preprocessing.channel_averager.ChannelAverager
-d3m.primitives.clustering.cluster_curve_fitting_kmeans.ClusterCurveFittingKMeans
-d3m.primitives.data_transformation.i_vector_extractor.IVectorExtractor
-d3m.primitives.data_transformation.segment_curve_fitter.SegmentCurveFitter
-d3m.primitives.data_transformation.sequence_to_bag_of_tokens.SequenceToBagOfTokens
-d3m.primitives.data_preprocessing.signal_dither.SignalDither
-d3m.primitives.time_series_segmentation.signal_framer.SignalFramer
-d3m.primitives.feature_extraction.signal_mfcc.SignalMFCC
-d3m.primitives.data_preprocessing.targets_reader.TargetsReader
-d3m.primitives.time_series_segmentation.uniform_segmentation.UniformSegmentation
-d3m.primitives.feature_extraction.tfidf_vectorizer.BBNTfidfTransformer
-d3m.primitives.classification.mlp.BBNMLPClassifier"
+primitives=$(python3.6 -m d3m.index search 2>/dev/null| grep d3m.primitives.*BBN)
+#primitives="d3m.primitives.data_preprocessing.csv_reader.CSVReader
+#d3m.primitives.data_preprocessing.audio_reader.AudioReader
+#d3m.primitives.data_preprocessing.channel_averager.ChannelAverager
+#d3m.primitives.clustering.cluster_curve_fitting_kmeans.ClusterCurveFittingKMeans
+#d3m.primitives.data_transformation.i_vector_extractor.IVectorExtractor
+#d3m.primitives.data_transformation.segment_curve_fitter.SegmentCurveFitter
+#d3m.primitives.data_transformation.sequence_to_bag_of_tokens.SequenceToBagOfTokens
+#d3m.primitives.data_preprocessing.signal_dither.SignalDither
+#d3m.primitives.time_series_segmentation.signal_framer.SignalFramer
+#d3m.primitives.feature_extraction.signal_mfcc.SignalMFCC
+#d3m.primitives.data_preprocessing.targets_reader.TargetsReader
+#d3m.primitives.time_series_segmentation.uniform_segmentation.UniformSegmentation
+#d3m.primitives.feature_extraction.tfidf_vectorizer.BBNTfidfTransformer
+#d3m.primitives.classification.mlp.BBNMLPClassifier"
 if [ $? -ne 0 ]; then
   echo "An error occured while getting the list of primitives" > /dev/stderr
   exit 1
